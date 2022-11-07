b2844d70f5dc310249dc60562a73e6571c636f4d
Stanysmaev <yarik231988@gmail.com>
2022-11-07 20:46:35 +0200

    hw16_fix

--- js/script.js
+++ js/script.js
@@ -17,3 +17,34 @@ $('.modal-wrapper, .modal-window .close').click(function(event) {
     $('.modal-wrapper').removeClass('active-new');
   }
 })
+
+$('#pills-1-tab').click(function() {
+  $('#pills-1-tab').addClass('active-urls-element');
+  $('#pills-2-tab').removeClass('active-urls-element');
+  $('#pills-3-tab').removeClass('active-urls-element');
+  $('#pills-4-tab').removeClass('active-urls-element');
+})
+
+$('#pills-2-tab').click(function() {
+  $('#pills-2-tab').addClass('active-urls-element');
+  $('#pills-1-tab').removeClass('active');
+  $('#pills-1-tab').removeClass('active-urls-element');
+  $('#pills-3-tab').removeClass('active-urls-element');
+  $('#pills-4-tab').removeClass('active-urls-element');
+})
+
+$('#pills-3-tab').click(function() {
+  $('#pills-3-tab').addClass('active-urls-element');
+  $('#pills-1-tab').removeClass('active');
+  $('#pills-1-tab').removeClass('active-urls-element');
+  $('#pills-2-tab').removeClass('active-urls-element');
+  $('#pills-4-tab').removeClass('active-urls-element');
+})
+
+$('#pills-4-tab').click(function() {
+  $('#pills-4-tab').addClass('active-urls-element');
+  $('#pills-1-tab').removeClass('active');
+  $('#pills-1-tab').removeClass('active-urls-element');
+  $('#pills-2-tab').removeClass('active-urls-element');
+  $('#pills-3-tab').removeClass('active-urls-element');
+})
